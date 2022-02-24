4130ac0accdbfe24f88641932bb39c59ef981c10
Envyxking <99601504+Envyxking@users.noreply.github.com>
2022-02-24 11:50:50 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -14,7 +14,7 @@ class script(object):
     SOURCE_TXT = """<b>NOTE:</b>
 - Sakura is not a open source project. 
 - but i will give my movie group link join and enjoy
-✯ 🎬 𝑴𝒐𝒗𝒊𝒆 𝒈𝒓𝒐𝒖𝒑:<a href=https://t.me/cinima_lokham>⚡️YAGAMI 🍿Movies And Series HOUSE🎥</a>
+✯ 🎬 𝑴𝒐𝒗𝒊𝒆 𝒈𝒓𝒐𝒖𝒑:<a href=https://t.me/+cg0c_N3yz0Q0N2E1>⚡️YAGAMI 🍿Movies And Series HOUSE🎥</a>
 
 <b>DEVS:</b>
 - <a href=https://t.me/Unni0240>Amal Nath</a>"""
