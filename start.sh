713a9d4e67d9f9eef7cc918093e53bc0221100c4
Envyxking <99601504+Envyxking@users.noreply.github.com>
2022-02-24 10:34:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Masterrockiei/Sakura.git /Sakura
+  git clone https://github.com/Envyxking/telebot /telebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Sakura
